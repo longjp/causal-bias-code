edfb6aceebef906ed01d6d4cb2e93f208d53dc97
James P Long <jplong@mdanderson.org>
2021-08-03 12:26:10 -0500

    removed unused code, improved commenting

--- analysis/funcs.R
+++ analysis/funcs.R
@@ -4,8 +4,7 @@ CVModel <- function(predictX,obs,ko,ix,rseed=rseed,mc.cores=4){
   out_predictX <- list()
   for(ii in 1:length(folds)){
     ko_train <- ko[ix!=ii,]
-    ko_test_names <- rownames(ko)[ix==ii]
-    out_predictX[[ii]] <- predictX(obs,ko_train,ko_test_names,rseed,mc.cores)
+    out_predictX[[ii]] <- predictX(obs,ko_train,rseed,mc.cores)
   }
   return(out_predictX)
 }
--- analysis/predictEST.R
+++ analysis/predictEST.R
@@ -1,3 +1,7 @@
+## fits estimators on CV subsets of Kemmeren
+## parallelizing computations across response genes y
+## (each of ~6000) genes is treated as a response once
+## implements bootstrap sampling
 library(parallel)
 library(glmnet)
 ## causalDantzig was removed from current version of InvariantCausalPrediction package. 
@@ -6,17 +10,19 @@ library(glmnet)
 #install_version("InvariantCausalPrediction", version = "0.7-1")
 library(InvariantCausalPrediction)
 
-predictEST <- function(obs,ko_train,ko_test_names,rseed,mc.cores=4){
+
+##           obs : observational data matrix with n_1 rows and p columns
+##      ko_train : knockout training data with n_2 rows and p columns
+##         rseed : random seed. this random seed is used to generate random seeds
+##                 for each response gene y
+##     mc.socres : number of cores to parallelize across
+predictEST <- function(obs,ko_train,rseed,mc.cores=4){
   set.seed(rseed)
   dat <- rbind(obs,ko_train)
   exp <- c(rep("obs",nrow(obs)),rep("int",nrow(ko_train)))
-  ## create E matrix
-  m1 <- matrix(0,nrow=nrow(obs),ncol=nrow(ko_train))
-  m2 <- diag(nrow(ko_train))
-  E <- rbind(m1,m2)
-  rownames(E) <- rownames(dat)
   ComputeFit <- function(ii,rseeds){
     set.seed(rseeds[ii])
+    ## summarize progress
     if(ii %% 100==0){
       print(paste0("run: ",ii,"/",N))
     }
@@ -25,7 +31,6 @@ predictEST <- function(obs,ko_train,ko_test_names,rseed,mc.cores=4){
     ix <- rownames(dat) != yname
     exp_sub <- exp[ix]
     X <- dat[ix,]
-    E <- E[ix,]
     Y <- X[,ii]  
     X <- X[,-ii]
     ## bootstrap sample training data
@@ -33,13 +38,12 @@ predictEST <- function(obs,ko_train,ko_test_names,rseed,mc.cores=4){
     exp_sub <- exp_sub[ix]
     Y <- Y[ix]
     X <- X[ix,]
-    E <- E[ix,]
     ## run lasso to select top predictors
     coefs <- coef(glmnet(X, Y))
     coefs_nonzero <- colSums(coefs!=0)
     coefs <- coefs[,which.min(abs(coefs_nonzero-5))[1]]
     ## create l1 estimate
-    l1_est <- coefs[-1]
+    l1_est <- coefs[-1] ## removes intercept
     l1_est <- l1_est[l1_est!=0]
     ## randomly permute coefficients to get l1 random
     l1r_est <- l1_est
@@ -49,10 +53,10 @@ predictEST <- function(obs,ko_train,ko_test_names,rseed,mc.cores=4){
     if(sum(ix)>0){
       Xsub <- X[,ix,drop=FALSE]
     } else {
-      ## select random columns if lasso does not return anything
+      ## select random columns if lasso does not return anything, unusual
       Xsub <- X[,sample(ncol(X),4)] 
     }
-    ## CD coeff
+    ## fit CD
     fit <- try(causalDantzig(Xsub,Y,ExpInd=exp_sub))
     if(class(fit)!="try-error"){
       fn <- names(coef(fit))
@@ -72,7 +76,6 @@ predictEST <- function(obs,ko_train,ko_test_names,rseed,mc.cores=4){
     Xsub <- NULL
     X <- NULL
     Y <- NULL
-    E <- NULL
     gc()
     tout <- list(l1_est=l1_est,
                  l1r_est=l1r_est,
@@ -87,6 +90,7 @@ predictEST <- function(obs,ko_train,ko_test_names,rseed,mc.cores=4){
   ## each iteration of mclapply has its own random starting seed
   ## this makes computations exactly reproducible
   rseeds <- sample(1:10^8,N)
+  ## each iteration treats one gene (1:N) as response
   out <- mclapply(1:N,ComputeFit,rseeds=rseeds,mc.cores=mc.cores)
   names(out) <- colnames(ko_train)[1:N]
   return(out)
